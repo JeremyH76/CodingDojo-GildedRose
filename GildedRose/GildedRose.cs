d21f1456b63b55b032689ce3e9823eed4d4bfb31
Jérémy Hure <hure.j@sfeir.com>
2022-10-20 11:05:08 +0200

    Inverse SellIn<0 if !AgedBrie !Backstage

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -43,20 +43,17 @@ namespace GildedRoseKata
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name == "Aged Brie")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            Items[i].Quality--;
-                        }
-                        else
-                        {
-                            Items[i].Quality = 0;
-                        }
+                        Items[i].Quality++;
+                    }
+                    else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        Items[i].Quality = 0;
                     }
                     else
                     {
-                        Items[i].Quality++;
+                        Items[i].Quality--;
                     }
                 }
                 if (Items[i].Quality < 0) { Items[i].Quality = 0; }
