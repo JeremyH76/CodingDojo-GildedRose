e35c984414cf1dec5f71ad20edbdf41222b1a851
Jérémy Hure <hure.j@sfeir.com>
2022-10-20 10:53:30 +0200

    Inverse if / else for Quality ++/--

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -15,14 +15,7 @@ namespace GildedRoseKata
             for (var i = 0; i < Items.Count; i++)
             {
                 if (Items[i].Name == "Sulfuras, Hand of Ragnaros") { continue; }
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        Items[i].Quality--;
-                    }
-                }
-                else
+                if (Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality < 50)
                     {
@@ -46,6 +39,14 @@ namespace GildedRoseKata
                                 }
                             }
                         }
+                    }
+                    
+                }
+                else
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        Items[i].Quality--;
                     }
                 }
 
